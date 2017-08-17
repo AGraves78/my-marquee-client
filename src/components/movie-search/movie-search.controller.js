5d438b4bd77bb51cb17d4d48269693887c4008fc
Angela Graves <algraves78@gmail.com>
2017-08-16 22:59:29 -0400

    set up http request to the movie service and modified the controller to match changes in the movie service method

--- src/components/movie-search/movie-search.controller.js
+++ src/components/movie-search/movie-search.controller.js
@@ -2,7 +2,9 @@ MainController.$inject = ['MovieService'];
 
 function MainController(movie){
   this.message = 'hello from angular';
-  this.movieData = movie.getMovieResults();
+
+  movie.getMovieResults('Jack Reacher')
+       .then(movieSearch => this.movieData = movieSearch);
 }
 
 module.exports = MainController;
--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,12 +1,17 @@
 const movieData = require('../../mocks/movie-search.json');
-MovieService.$inject = [];
+MovieService.$inject = ['$http'];
 
-function MovieService(){
+function MovieService($http){
+  const baseUrl = 'https://my-marquee.herokuapp.com/search/';
   return {
     getMovieResults: getMovies
   };
-  function getMovies(){
-    return movieData.results;
+  function getMovies(movie){
+    const url = `${baseUrl}${movie}`;
+    return $http.get(url)
+                .then(response => {
+                  return response.data.results;
+                });
   }
 }
 
